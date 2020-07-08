04ac461315174355731afe48cebb2b5bd9b3503c
jrpbjr <roberto@inosystem.com.br>
2020-07-08 13:54:21 -0300

    habilitar o spring webservice

--- src/main/java/com/server/soap/webservices/customersadministration/soap/WebServiceConfig.java
+++ src/main/java/com/server/soap/webservices/customersadministration/soap/WebServiceConfig.java
@@ -1,9 +1,11 @@
 package com.server.soap.webservices.customersadministration.soap;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 
 @Configuration
+@EnableWs
 public class WebServiceConfig extends WsConfigurerAdapter {
 
 }
