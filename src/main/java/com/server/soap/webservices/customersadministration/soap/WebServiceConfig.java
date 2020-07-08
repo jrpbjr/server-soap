5a0fcd9675554342ede76acf33e80226b71d3263
jrpbjr <roberto@inosystem.com.br>
2020-07-08 14:04:42 -0300

    controlador central de requisiçao que chega ao spring eprocessaso primeiro pelo dispacher identifica o controler que vai tratar a requisição. servico simplificado de mensagem de webservice, trata a solicitações soap.

--- src/main/java/com/server/soap/webservices/customersadministration/soap/WebServiceConfig.java
+++ src/main/java/com/server/soap/webservices/customersadministration/soap/WebServiceConfig.java
@@ -1,11 +1,19 @@
 package com.server.soap.webservices.customersadministration.soap;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+import javax.servlet.ServletRegistration;
 
 @Configuration
 @EnableWs
 public class WebServiceConfig extends WsConfigurerAdapter {
 
+    public ServletRegistration messageDispatcherServlet(ApplicationContext){
+        MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+
+    }
 }
