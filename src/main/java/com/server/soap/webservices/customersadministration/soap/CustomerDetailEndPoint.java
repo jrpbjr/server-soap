3acb54ca765bce82407b10b8114fd76e90fbab9e
jrpbjr <roberto@inosystem.com.br>
2020-07-08 09:05:51 -0300

    JAXB 2 	para fazer binding para o objeto java xml para a ser java

--- src/main/java/com/server/soap/webservices/customersadministration/soap/CustomerDetailEndPoint.java
+++ src/main/java/com/server/soap/webservices/customersadministration/soap/CustomerDetailEndPoint.java
@@ -4,12 +4,13 @@ import br.com.serversoap.GetCustomerDetailRequest;
 import br.com.serversoap.GetCustomerDetailResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 
 @Endpoint
 public class CustomerDetailEndPoint {
 
     @PayloadRoot(namespace = "http://serversoap.com.br", localPart = "GetCustomerDetailRequest")
-    public GetCustomerDetailResponse processCustomerDetailRequest(GetCustomerDetailRequest req){
+    public GetCustomerDetailResponse processCustomerDetailRequest(@RequestPayload GetCustomerDetailRequest req){
         GetCustomerDetailResponse response = new GetCustomerDetailResponse();
         return response;
     }
