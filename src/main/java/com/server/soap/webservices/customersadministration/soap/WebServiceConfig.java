30004727ed676fd65dbd235466e4b6a41a63f032
jrpbjr <roberto@inosystem.com.br>
2020-07-08 14:31:23 -0300

    configuração para fazer uso do xsd,criar extancia para retorno

--- src/main/java/com/server/soap/webservices/customersadministration/soap/WebServiceConfig.java
+++ src/main/java/com/server/soap/webservices/customersadministration/soap/WebServiceConfig.java
@@ -8,6 +8,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
@@ -26,5 +27,12 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema customerSchema(){
         return new SimpleXsdSchema(new ClassPathResource("customer-information.xsd"));
+    }
+
+    @Bean(name = "customers")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema customerSchema){
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+
+        return definition;
     }
 }
