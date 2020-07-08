905649bbb50bad56ba766fb98032084faa9a276f
jrpbjr <roberto@inosystem.com.br>
2020-07-08 09:01:54 -0300

    metodo para processar e retornar a solicitação preparado para receber o xml  definido

--- src/main/java/com/server/soap/webservices/customersadministration/soap/CustomerDetailEndPoint.java
+++ src/main/java/com/server/soap/webservices/customersadministration/soap/CustomerDetailEndPoint.java
@@ -1,4 +1,17 @@
 package com.server.soap.webservices.customersadministration.soap;
 
+import br.com.serversoap.GetCustomerDetailRequest;
+import br.com.serversoap.GetCustomerDetailResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+
+@Endpoint
 public class CustomerDetailEndPoint {
+
+    @PayloadRoot(namespace = "http://serversoap.com.br", localPart = "GetCustomerDetailRequest")
+    public GetCustomerDetailResponse processCustomerDetailRequest(GetCustomerDetailRequest req){
+        GetCustomerDetailResponse response = new GetCustomerDetailResponse();
+        return response;
+    }
+
 }
