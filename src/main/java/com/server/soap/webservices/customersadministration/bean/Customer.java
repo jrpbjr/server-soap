419f3f398223bb9436dfafd7a5c34d5886186c0a
jrpbjr <roberto@inosystem.com.br>
2020-07-08 16:53:21 -0300

    Bean criado.

--- src/main/java/com/server/soap/webservices/customersadministration/bean/Customer.java
+++ src/main/java/com/server/soap/webservices/customersadministration/bean/Customer.java
@@ -1,4 +1,52 @@
 package com.server.soap.webservices.customersadministration.bean;
 
 public class Customer {
+    private Integer id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Customer(Integer id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer [id=%s, name=%s, phone=%s, email=%s]", id, name, phone, email);
+    }
 }
