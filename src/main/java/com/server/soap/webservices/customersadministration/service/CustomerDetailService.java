13bcc6f29448062b326fa14bd4fa1c558de52b5f
jrpbjr <roberto@inosystem.com.br>
2020-07-08 22:15:42 -0300

    lista para manter em memoria para teste.

--- src/main/java/com/server/soap/webservices/customersadministration/service/CustomerDetailService.java
+++ src/main/java/com/server/soap/webservices/customersadministration/service/CustomerDetailService.java
@@ -1,8 +1,31 @@
 package com.server.soap.webservices.customersadministration.service;
 
+import com.server.soap.webservices.customersadministration.bean.Customer;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Component
 public class CustomerDetailService {
+    private static List<Customer> customers = new ArrayList<>();
+
+    static{
+        Customer customer1 = new Customer(1,"Bob", "99999999", "bob@gmail.com");
+        customers.add(customer1);
+
+        Customer customer2 = new Customer(2,"Mark", "8888888", "mark@gmail.com");
+        customers.add(customer2);
+
+        Customer customer3 = new Customer(3,"Jose", "7777777", "jose@gmail.com");
+        customers.add(customer3);
+
+        Customer customer4 = new Customer(4,"Klay", "5555555", "klay@gmail.com");
+        customers.add(customer4);
+
+
+
+    }
 
 }
