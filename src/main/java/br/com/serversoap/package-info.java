ec2e3beaa6576ff7b52caa760aeb161042f52302
jrpbjr <roberto@inosystem.com.br>
2020-07-08 23:26:07 -0300

    alteração no xsd maven clean e install para gerar as classes deletadas

--- src/main/java/br/com/serversoap/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
-// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2020.07.07 às 11:49:56 PM BRT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://serversoap.com.br", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package br.com.serversoap;
