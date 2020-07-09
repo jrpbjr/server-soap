c0c18d5799b881e0e1f413ca9d43f86350e9bc93
jrpbjr <roberto@inosystem.com.br>
2020-07-09 16:21:13 -0300

    tratamento de exceções customizada,podendo elaborar uma tabela de erro. boa resposta para quem consome o serviço. resposta de falha.

--- src/main/java/com/server/soap/webservices/customersadministration/exception/CustomerNotFoundException.java
+++ src/main/java/com/server/soap/webservices/customersadministration/exception/CustomerNotFoundException.java
@@ -3,7 +3,7 @@ package com.server.soap.webservices.customersadministration.exception;
 import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
 import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
 
-@SoapFault(faultCode = FaultCode.CLIENT)
+@SoapFault(faultCode = FaultCode.CUSTOM, customFaultCode = "{http://serversoap.com.br}001_Customer_not_Found")
 public class CustomerNotFoundException extends RuntimeException{
 
     private static final long serialVersionUID = 1L;
