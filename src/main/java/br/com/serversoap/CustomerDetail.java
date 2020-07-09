be70d61841a0a697a0f6533a548fbf028c6d975e
jrpbjr <roberto@inosystem.com.br>
2020-07-09 17:59:04 -0300

    o rebuild maven gerou objetos novamente. adicionei a dependencia javax.activation dependencia para segurança wss

--- src/main/java/br/com/serversoap/CustomerDetail.java
+++ src/main/java/br/com/serversoap/CustomerDetail.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2020.07.09 às 09:43:58 AM BRT 
+// Gerado em: 2020.07.09 às 05:32:14 PM BRT 
 //
 
 
