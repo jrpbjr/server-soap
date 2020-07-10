750aaea7c46a2b6819b9591d0187ab80141a8d42
jrpbjr <roberto@inosystem.com.br>
2020-07-10 19:18:08 -0300

    anotações jpa @Id referente ao id da tabela, geração automatica.

--- src/main/java/com/server/soap/webservices/customersadministration/bean/Customer.java
+++ src/main/java/com/server/soap/webservices/customersadministration/bean/Customer.java
@@ -1,11 +1,17 @@
 package com.server.soap.webservices.customersadministration.bean;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigInteger;
 
 @Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
+
     private String name;
     private String phone;
     private String email;
